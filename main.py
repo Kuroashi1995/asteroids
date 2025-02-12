7024c831cabebd301470021225f0b5bc5ae19667
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-02-12 14:43:29 -0300

    Player triangle now renders on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
 import pygame
+from player import *
 
 def main():
     pygame.init()
@@ -8,6 +9,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +17,7 @@ def main():
 
         print(f"Game running in {SCREEN_WIDTH}x{SCREEN_HEIGHT}")
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         print(dt)
