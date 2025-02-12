3d49f621ffbfc2b2b933654d3408886150470f2d
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-02-12 11:38:46 -0300

    minor ignore changes, close button works now

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     print("Starting asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         print(f"Game running in {SCREEN_WIDTH}x{SCREEN_HEIGHT}")
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
