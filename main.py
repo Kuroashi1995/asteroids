6d663588f1812d65acccb7365cf5cfa8d4054a5b
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-02-12 11:52:43 -0300

    game runs at 60 fps now

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     pygame.init()
     print(pygame.get_init())
     print("Starting asteroids!")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -14,5 +16,8 @@ def main():
         print(f"Game running in {SCREEN_WIDTH}x{SCREEN_HEIGHT}")
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
+
 if __name__ == "__main__":
     main()
